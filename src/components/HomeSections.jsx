b92925f97d093f1b7ebbb1466dda4c35ea0a588a
imkashishkathuria <kathuriak805@gmail.com>
2025-08-24 20:14:07 +0530

    sections font size update

--- src/components/HomeSections.jsx
+++ src/components/HomeSections.jsx
@@ -4,7 +4,7 @@ const HomeSections = ({ heading, subheading, cards, gap }) => {
   return (
     <div className={`flex flex-col gap-3 md:gap-${gap} justify-center items-center`}>
         <h1 className='text-[#0A1A2F] text-center font-semibold md:font-normal md:text-left text-[25px] md:text-[45px]'>{heading}</h1>
-        <p className='text-[12px] md:text-[17px] text-[#333333] text-center md:w-[940px]'>{subheading}</p>
+        <p className='text-[10px] md:text-[17px] text-[#333333] w-[300px] text-center md:w-[940px]'>{subheading}</p>
         <div>{cards}</div>
         <div className='hidden md:flex gap-7'>
             <img src='/Frame 237997.png' className='w-[40px] h-[40px] cursor-pointer' />
