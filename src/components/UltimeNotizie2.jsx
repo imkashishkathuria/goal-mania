15c41176e6ee2c002ae5e44c981e135fb2e279ca
imkashishkathuria <kathuriak805@gmail.com>
2025-08-27 20:50:11 +0530

    video section scrollbar working for mobile and desktop

--- src/components/UltimeNotizie2.jsx
+++ src/components/UltimeNotizie2.jsx
@@ -4,15 +4,19 @@ import HomeSections from './HomeSections'
 const UltimeNotizie2 = () => {
   return (
     <div>
-    <div className='hidden md:block py-7 justify-between items-center'>
+    <div className='py-7 justify-between items-center'>
       <HomeSections heading="Ultime Notizie" subheading="Scopri le notizie più fresche e importanti dal mondo del calcio, aggiornate in tempo reale." gap="5" cards={
-        <img src='/Frame 237936.png' />
+        <div className='flex gap-1 pl-3 md:pl-0'>
+          <img src='/Frame 237934.png' className='w-full h-[350px] md:h-[500px] object-contain' />
+           <img src='/Frame 237933.png' className='w-full h-[350px] md:h-[500px] object-contain'/>
+            <img src='/Frame 237936 (1).png' className='w-full h-[350px] md:h-[500px] object-contain'/>
+             <img src='/Frame 237935.png' className='w-full h-[350px] md:h-[500px] object-contain'/>
+              <img src='/Frame 237938.png' className='w-full h-[350px] md:h-[500px] object-contain' />
+          </div>
       } />
       
       </div>
-      <div className='md:hidden py-5 px-4'>
-        <img src='/Frame 237851.png' />
-      </div>
+      
     </div>
   )
 }
